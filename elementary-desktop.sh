876ee53ea1561f31e1eb10b80099bea68377f4e9
Ming Choi <mingchoi.na@gmail.com>
2018-12-16 08:47:01 +0800

    Updated elementary-desktop

--- elementary-desktop.sh
+++ elementary-desktop.sh
@@ -1,3 +1,4 @@
 sudo apt-add-repository ppa:elementary-os/stable
 sudo apt update
 sudo apt install elementary-desktop
+gsettings set org.pantheon.desktop.gala.appearance button-layout :minimize,maximize,close
